531d94e10999e199939c6c4a25e52cba6502aa8a
JimmyAllDay <jameswhmarshall@gmail.com>
2021-04-28 12:09:51 +1000

    functioning post call updates UI

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// ----------------- Project Set Up ------------------------
+// Endpoint
+let appData = [];
 // add express
 const express = require('express');
 // add path
@@ -6,18 +9,29 @@ const path = require('path');
 const cors = require('cors');
 // import node-fetch
 const fetch = require('node-fetch');
+// import body-parser
+const bodyparser = require('body-parser');
 // create a new instance of express
 const app = express();
 // set port
 const port = 3000;
 // import logger
 const logger = require('./public/middleware/logger');
-// import response module
+// import GET response module
 // const response = require('./public/js/response.js');
-// API Key
 
+// -------------------- API Variables ---------------------
+// Base URL
+const apiBaseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+// API Key
+const apiKey = '4642a132e4fdbcbc35d525e854c0c51c';
+
+// ----------------- Initialise Middleware ------------------
 // init cors
 app.use(cors());
+
+// init body parser
+app.use(bodyparser.json());
 
 // init logger middleware
 // app.use(logger);
@@ -25,16 +39,31 @@ app.use(cors());
 // Set static folder for serving html
 app.use(express.static(path.join(__dirname, 'public')));
 
+// -------------------Server Operations ---------------------
+
 // callback to show server is listening
 app.listen(port, () => {
   console.log(`app is listenting on port ${port}`);
 });
 
-// POST Route to handle user input
-app.post('/post', (req, res) => {
-  let apiString = 'Thanks, ya filthy animal';
-  console.log('post data received');
-  res.send(apiString);
+// POST Route to collect user input
+app.post('/postData', (req, res) => {
+  // Send input to project endpoint array
+  data = req.body;
+  appData.push(data);
+  // API URL
+  let apiURL = `${apiBaseUrl}?key=${apiKey}&txt=${appData[0].answer}&lang=auto`;
+  async function getsentiment() {
+    await fetch(apiURL)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        res.send(data);
+      });
+  }
+  getsentiment();
 });
 
 // response to get request on '/response' path
@@ -48,7 +77,6 @@ app.get('/response', (req, res) => {
       .then(data => {
         responseData = data;
       });
-    console.log(responseData);
     return res.send(responseData);
   }
   response();
--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,38 @@ let formValue = '';
 const form = document.getElementById('form-input-field');
 const button = document.getElementById('submit');
 const serverMessage = document.getElementById('server-message');
+const userPost = 'http://localhost:3000/postData';
 
 // ------------------ Post Request to Node ---------------
 
 button.addEventListener('click', function() {
-  formValue = form.value;
+  answer = formValue = form.value;
   console.log(form.value);
+
+  // Note - this code is taken directly from the Mozilla documentation
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+
+    return await response.json();
+  }
+
+  postData(userPost, { answer: formValue })
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+      serverMessage.innerText = data.irony;
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 // ---------------- GET Request from Node ----------------
@@ -27,4 +53,4 @@ async function updateServerMessage() {
       console.log(error);
     });
 }
-updateServerMessage();
+// updateServerMessage();
